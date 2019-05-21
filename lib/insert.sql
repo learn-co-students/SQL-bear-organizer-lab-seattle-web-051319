db1dae0b5a1a20150eb36e89c2987fa8824279c3
AustinBH <harlowaustin2@gmail.com>
2019-05-21 13:40:22 -0700

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr. Chocolate", 15, "M", "caramel", "rowdy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Rowdy", 8, "M", "grey", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Tabitha", 21, "F", "sandy brown", "relaxed", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Sergeant Brown", 12, "F", "orange", "firm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Melissa", 14, "F", "blonde", "happy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Grinch", 19, "M", "green", "mean", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Wendy", 4, "F", "dark brown", "energetic", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+(NULL, 24, "M", "grey-black", "vicious", 1);

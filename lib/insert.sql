7a96b3a7bd1f3157913ecf844e2ed0a20926acf4
Noah Fairbairn <noahfairbairn@gmail.com>
2019-05-22 13:42:22 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (id, name, age, gender, temperament, alive)
+VALUES (1, "Mr.Chocolate", 10, "M", "grumpy", true);
+
+INSERT INTO bears (id, name, age, gender, temperament, alive)
+VALUES (2, "Rowdy", 12, "M", "rowdy", true);
+
+INSERT INTO bears (id, name, age, gender, temperament, alive)
+VALUES (3, "Tabitha", 9, "F", "sweet", true);
+
+INSERT INTO bears (id, name, age, gender, temperament, alive)
+VALUES (4, "Sargeant Brown", 20, "M", "stern", true);
+
+INSERT INTO bears (id, name, age, gender, temperament, alive)
+VALUES (5, "Melissa", 12, 'F', "passive aggressive", true);
+
+INSERT INTO bears (id, name, age, gender, temperament, alive)
+VALUES (6, "Grinch", 25, "M", "grinchy", true);
+
+INSERT INTO bears (id, name, age, gender, temperament, alive)
+VALUES (7, "Wendy", 17, 'F', "4for4", true);
+
+INSERT INTO bears (id, name, age, gender, temperament, alive)
+VALUES (8, NULL, 10, "M", "grumpy", true);
